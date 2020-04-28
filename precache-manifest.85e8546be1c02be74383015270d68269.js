453b4c1140b9404465af55db4fe63d6142a4b530
Alena Kernazhytskaya <akernazhytskaya@1stquad.com>
2020-04-28 21:46:13 +0200

    Updates

---
+++ precache-manifest.85e8546be1c02be74383015270d68269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8274dd5d63edd304f97e40dc9c135b6c",
+    "url": "/stories/index.html"
+  },
+  {
+    "revision": "686cd91348e8e3c4f86e",
+    "url": "/stories/static/css/main.7daf72b5.chunk.css"
+  },
+  {
+    "revision": "8af4bbc564ef03799e2e",
+    "url": "/stories/static/js/2.12d0a68b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/stories/static/js/2.12d0a68b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686cd91348e8e3c4f86e",
+    "url": "/stories/static/js/main.077c8e74.chunk.js"
+  },
+  {
+    "revision": "7e59dff6ae95c6b27175",
+    "url": "/stories/static/js/runtime-main.97b35874.js"
+  }
+]);
